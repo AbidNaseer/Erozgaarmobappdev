34affbccdfac50253d6569c073788294f2e33b79
Abid Naseer <abidnaseer454@gmail.com>
2022-03-29 20:48:55 +0500

    done

---
+++ Dart/class1.dart
@@ -0,0 +1,16 @@
+void main() {
+  var my_Class = "This is the first variable";
+  print(my_Class);
+  // it is a simple script with single line comment
+  // yeh 1 sada sa program hai jo myclass variable ki value ko dikha rha hai
+
+  var $number1 = 34;
+  var $number2 = 18.07;
+  var $plus = ($number1 + $number2);
+  print($plus);
+/*
+yeh b 1 sada sa script hai jis mein 2 mukhtalif noiyat k vavriables shamil hain  
+$number1 int hai 
+jab k number2 float hai (isko float is liye kaha jata hau q k yeh ishariaya k sath hain)
+*/
+}
