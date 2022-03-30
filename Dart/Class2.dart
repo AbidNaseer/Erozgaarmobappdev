38218addc053ed75ad24f0d779d2c086c4949d61
Abid Naseer <abidnaseer454@gmail.com>
2022-03-30 14:07:30 +0500

    done

---
+++ Dart/Class2.dart
@@ -0,0 +1,73 @@
+void main() {
+//   here is the list of camelCase variables
+  var $myName = "Abid Naseer",
+      $fatherName = "Naseer Ahmed",
+      $livingPlace = "Faislabad",
+      $occupationType = "freelancer",
+      $nationalityHolder = "Pakistani",
+      $monthlyIncome = "0",
+      $religionIslam = "true",
+      $familyMembers = "10",
+      $voterId = "12",
+      $criminalRecord = "null";
+
+  //   here is the list of snack_case variables
+  var fruit_1 = "Apple";
+  var fruit_2 = "Banan";
+  var fruit_3 = "Grapes";
+  var fruit_4 = "mangoes";
+  var fruit_5 = "peach";
+  var fruit_6 = "Lichi";
+  var fruit_7 = "Guava";
+  var fruit_8 = "Strawberry";
+  var fruit_9 = "PineApple";
+  var fruit_10 = "Watemelon";
+
+  // here is exapmles of 10 string variales
+  String $nation = "Pakistan";
+  String $national_flag = "Sabz Halali";
+  String $national_flag_color = "Green White";
+  String $national_fruit = "Mango";
+  String $national_juice = "ganna ka Russ";
+  String $national_crop = "Gandom";
+  String $nations_main_Occupation = "Agriculture";
+  String $national_dress = "Shalwar Qameez";
+  String $national_aim = "IMAN ITIHAAD TANZEEM";
+  String $national_song = "PAk Sar Zameen";
+
+//   here is the examples of integer variables
+  int national_area = 796096;
+  int national_population = 22000000;
+  int national_provinces = 5;
+  int national_languages = 1;
+  int national_forces = 3;
+  int national_srcurity_budget = 17389545174;
+  int national_eductional_budget = 65239853;
+  int national_developmetal_budget = 69658745;
+  int national_judiciry_budget = 26589541;
+  int international_ranked_in_justice_system = 28;
+
+  //   here is the example of floatn or dbl type in Dart
+  double num_one = 1.1;
+  double num_two = 2.2;
+  double num_three = 3.3;
+  double num_four = 2.4;
+  double num_five = 2.26;
+  double num_six = 2.82;
+  double num_seven = 2.92;
+  double num_eight = 2.362;
+  double num_nine = 2.9652;
+  double num_ten = 2.1452;
+
+  // here is the exapmle of bool_type in Dart
+  bool $nation_Pakistan = true;
+  bool $flag = true;
+  bool $flag_color_green = true;
+  bool $fruit_Mango = true;
+  bool $juice_gannakaRuss = true;
+  bool $fasal_Chana = false;
+  bool $main_Occupation_Agriculture = true;
+  bool $dress_ShalwarQameez = true;
+  bool $aim_unity = true;
+  bool $song_paksarzamen = true;
+}
