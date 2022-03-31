d8d96fcc3dc595be46f5b4e37ba2d66c0d71407c
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-03-31 21:58:08 +0500

    Add files via upload

---
+++ Dart/class3.dart
@@ -0,0 +1,25 @@
+void main() {
+  var name = "Jane";
+  print(name);
+
+  String? address;
+  address;
+  print(address);
+
+  var birthYear = 1996;
+  var futureYear = 2050;
+  print("I will be ${birthYear - futureYear} in $futureYear");
+
+  int value_one = 10;
+  int value_two = 20;
+  print("Additon of the  both values ${value_one + value_two}");
+  print("Division of the  both values ${value_one / value_two}");
+  print("Multiplication of the  both values ${value_one * value_two}");
+  print("Subtraction of the  both values ${value_one - value_two}");
+
+  int celsius = 2;
+  print(celsius.toString() + "is farenhight");
+
+  int f = 2;
+  print(f.toString() + "is celsius");
+}
