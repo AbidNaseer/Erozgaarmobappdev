892c9b137b6e357514598ff21139f0eff6aa5842
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-04-07 14:20:59 +0500

    Add files via upload

---
+++ Dart/class7.dart
@@ -0,0 +1,62 @@
+// Solution of the 1st problem //
+int a = 0;
+  int b = 0;
+  if(a == b ){
+    print("a is equal to the b");
+  } else{
+    print("a is not equal to the b ");
+  };
+// This is the solution of 2nd problem//
+int rollno_1 = 15;
+int rollno_2 = 25;
+  if (rollno_1 > rollno_2){
+    print ("a is greater than b");
+  } else {
+    print("a is not greater than b ");
+  }
+
+// This is the solution of 3rd  problem//
+var age = 0;
+  if (age >= 18 && age < 60){
+    print("you can cast the vote ");
+  } else {
+    print("stay home stay safe ");
+  };
+
+
+// This is the solution of 4th problem //
+ int a = 100;
+int b = 50;
+int c = 30;
+  if(a > b && a > c){
+    print ("a is greater ");
+    } else if (b > a && b > c){
+    print( "b is greater");
+  }else {
+    print("c is the greater");
+  };
+// This is the solution of 5th problem //
+int number = 1;
+  if(number > 0){
+    print("it's a positive number");
+  } else if (number == 0 ){
+    print("this is zero");
+  } else{
+    print("it is negitive number");
+  };
+
+
+
+
+// this is the solution of 6th problem//
+int marks = 69;
+  if(marks >= 90 && marks <= 99){
+    print("A+");
+  } else if(marks >= 80 && marks <= 89){
+    print("A");
+  } else if(marks >= 70 && marks <= 79){
+    print("B");
+  } else{
+    print("better luck next time");
+  };
+
