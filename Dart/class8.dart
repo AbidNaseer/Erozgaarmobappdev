8675530f76a2f1c374400c3348de8cb7a936b9cd
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-04-12 09:22:58 +0500

    Add files via upload

---
+++ Dart/class8.dart
@@ -0,0 +1,48 @@
+void main() {
+  // this is the solution of first problem
+  var no_1 = 1;
+  for (var no_1 = 1; no_1 < 10; no_1++) {
+    print("the sum of ${no_1}");
+  }
+// this is the solution of second problem
+  var no_1 = 1;
+  for (var no_1 = 1; no_1 < 10; no_1++) {
+    print("the sum of ${no_1}");
+  }
+
+  print("the loop is finish now");
+
+// this is the solution of 3rd problem
+  var noOne = 5;
+  for (var i = 1; i < 11; i++) {
+    print("5 * ${i} = ${noOne * i}");
+  }
+
+// this is the solution of 4th  problem
+
+  var no = 10;
+
+  for (var i = 0; i < 10; i++) {
+    print("the reverse ${no--}");
+  }
+  // this is the solution of problem no 5
+  var noOne = 2;
+  for (var i = 1; i < 101; i++) {
+    if (i % 2 == 0) {
+      print("${i}");
+    }
+  }
+  // this is the solution of problem no 6
+  var noOne = 2;
+  for (var i = 1; i < 101; i++) {
+    if (i % 2 == 1) {
+      print("${i}");
+    }
+
+    // this is the solution of 7th problem
+    var noOne = 2;
+    for (var i = 1; i < 101; i++) {
+      print("${i + i}");
+    }
+  }
+}
