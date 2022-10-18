a91e1008e8591175dd5c2405a78657c40ace2509
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-10-17 23:34:35 -0700

    Add files via upload

---
+++ Class_04.dart
@@ -0,0 +1,18 @@
+void main(){
+//  here are 3 types of variable declaration type
+//Upper Camel Case
+//lowerCamelCase
+//Snack_Case
+// var AbidNaseer = "Abid NAseer";
+// var abidNaseer = "Abid NAseer";
+// print(AbidNaseer);
+//it is infred type of variable declartion
+
+var studentName = "Abid Naseer";
+var studentClass = 3;
+var studentRollNo = 25;
+print(studentClass);
+print(studentRollNo);
+print(studentName);
+
+}
