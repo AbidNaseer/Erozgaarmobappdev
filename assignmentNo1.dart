3d0ca5ba509b76f25d8ae58293b9547aba70d9fa
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-10-20 11:21:55 -0700

    Add files via upload
    this is the assignement of Dart

---
+++ assignmentNo1.dart
@@ -0,0 +1,25 @@
+void main() {
+  // problem no 1
+  String hello1 = "hello Dart";
+  print(hello1);
+// problem no 2
+  var houseNo = 45, postalAddress = 786;
+  print(houseNo);
+  print(postalAddress);
+// problem no 3
+  String city = "Faisalabad", state = "Punjab";
+  var home = "my home is situated at the cannal " +
+      "my city name is $city " +
+      "my state  name is $state";
+  print(home);
+// problem no 4
+  String firstName = "Abid Naseer", lastName = "Naseer Ahmad";
+  var fullName = "my name is $firstName " + "My Father  name is $lastName";
+  print(fullName);
+// problem no 5
+  var name = "jane";
+  print(name);
+// problem no 6
+  var address;
+  print(address);
+}
