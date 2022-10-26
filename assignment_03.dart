0388da71a57a7fbb625552a6f1741ed829fc66b8
Abid Naseer <95341916+AbidNaseer@users.noreply.github.com>
2022-10-26 11:52:46 -0700

    Add files via upload

---
+++ assignment_03.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Smart code completion (Ctrl + Shift + Space)
+  // Search everywhere (Double Shift)
+  // Show intention actions and quick-fixes (Alt+Enter)
+  // Save all (Ctrl+S)
+  // Open Settings dialog (Ctrl+Alt+S)
+  // Basic code completion (Ctrl+Space)
+  // Smart code completion (Ctrl+Shift+Space)
+  // Go to file (Ctrl+Shift+N)
+  // Copy (F5)
+  // Move (F6)
+  //  Modifier in Dart?
+  //  Here Are 3 Modifiers in dart
+  Const:
+  ;
+// Const is use for assigning the value to the variable that cannot be changed after its declared;
+//Const shows the error on Compile time that helps us to find the problem before code execution and save our time
+// All the Const are Finals
+  Final:
+  ;
+//Final has same functioning as Const with minimal changes like it shows the error on run time!
+//Final are immutable
+// but not all the Finals are Consts
+  Late:
+  ;
+//A late modifeir use in dart while declaring the non-nullable variable that initilized after it declaration;
+
+// if else statements are use for Control flow of the program
+//it determine's the flow of program like
+//where we want to check the statements and excute them it proves a signal lights for our proram's flow
+}
